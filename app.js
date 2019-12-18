dbe879254d09104247d3b812df52cc9ebc78e71b
손창우 <ckddn9496@gmail.com>
2019-12-18 15:56:17 +0900

    divide files using MVC pattern

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import morgan  from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+// import { userRouter } from "./routers/userRouter" // export default 로 하지 않으면 {}
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+const app = express();
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(helmet());
+app.use(morgan("dev")); // param으로 logging 방법이 바뀐다.
+
+// global router: join, login, home("/"), search...
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
+
+export default app; // 누군가 내 파일을 불러올 때 app object를 뱉어준다.
