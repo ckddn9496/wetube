62765e1bf232b768ae30ec0ae73620b5fff422ea
손창우 <ckddn9496@gmail.com>
2019-12-18 16:35:57 +0900

    make controller

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ import bodyParser from "body-parser";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
+
 const app = express();
 
 app.use(cookieParser());
@@ -16,8 +18,8 @@ app.use(helmet());
 app.use(morgan("dev")); // param으로 logging 방법이 바뀐다.
 
 // global router: join, login, home("/"), search...
-app.use("/", globalRouter);
-app.use("/users", userRouter);
-app.use("/videos", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app; // 누군가 내 파일을 불러올 때 app object를 뱉어준다.
